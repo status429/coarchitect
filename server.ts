d824da0e1bd0c76af471433211398308aef94295
Mert Doğaray <mertdogaray@Merts-MacBook-Air.local>
2025-12-06 00:21:42 +0300

    chore : initial commit

---
+++ server.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
