cdf5ee7837d90f74ec4a449225442c96f35087f8
Mert Doğaray <mertdogaray@Merts-MacBook-Air.local>
2025-12-07 12:31:47 +0300

    chore(backend) : implement multer package

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import path from 'path';
+import DXF_Routes from './routes/DXF_Routes';
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ app.use(express.json());
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// DXF Routes
+app.use('/api/dxf', DXF_Routes);
 
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
